1c42ca62f230dea1ffe6bb080c6f1a5a823dfdfe
Anzormumladze <46164149+Anzormumladze@users.noreply.github.com>
2020-03-08 20:54:04 +0400

    deleting items in todo (#2)
    * created todo app with vanila js added file structure and adding items logic
    
    * change file structure
    
    * fix scope
    
    * deleting items on click
    
    * cleaning code in html
    
    * delete fix
    
    * remove func done
    
    * changing let to const variables
    
    * fix
    
    * changed
    
    * remove app.css file
    
    * fix issues

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-let userInput = document.querySelector("#userInput");
+const userInput = document.querySelector("#userInput");
 
 const addItemHandler = event => {
   event.preventDefault();
 
-  let list = document.querySelector("#list");
-  let listItem = document.createElement("li");
+  const list = document.querySelector("#list");
+  const listItem = document.createElement("li");
+  const buttonDELETE = document.createElement("button");
 
   if (userInput.value.trim() === "") {
     alert("შეიყვანე რამე");
   } else {
     listItem.textContent = userInput.value;
+    buttonDELETE.textContent = "DELETE";
+    listItem.append(buttonDELETE);
     list.append(listItem);
   }
+
+  buttonDELETE.addEventListener("click", deleteMovieHandler);
+};
+
+const deleteMovieHandler = e => {
+  e.target.parentNode.remove();
 };
