2229167f4d3d51ddcfe4b25078c42d681e851280
Anzormumladze <anzor.anzor.mumladze@gmail.com>
2020-03-09 18:38:01 +0400

    rename items name

--- app.js
+++ app.js
@@ -6,19 +6,25 @@ const addItemHandler = event => {
   const list = document.querySelector("#list");
   const listItem = document.createElement("li");
   const buttonDELETE = document.createElement("button");
-
+  const buttonEDIT = document.createElement("button");
   if (userInput.value.trim() === "") {
     alert("შეიყვანე რამე");
   } else {
     listItem.textContent = userInput.value;
     buttonDELETE.textContent = "DELETE";
-    listItem.append(buttonDELETE);
+    buttonEDIT.textContent = "EDIT";
+    listItem.append(buttonDELETE, buttonEDIT);
     list.append(listItem);
   }
 
   buttonDELETE.addEventListener("click", deleteMovieHandler);
+  buttonEDIT.addEventListener("click", renameItemHander);
 };
 
 const deleteMovieHandler = e => {
   e.target.parentNode.remove();
+};
+
+const renameItemHander = e => {
+  e.target.parentNode.firstChild.textContent = prompt("change items name");
 };
