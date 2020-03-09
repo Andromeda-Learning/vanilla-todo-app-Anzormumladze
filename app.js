cbe27b547f8223a22b19a72c3a6e200073274410
Anzormumladze <anzor.anzor.mumladze@gmail.com>
2020-03-09 21:47:52 +0400

    rename items on button click

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ const addItemHandler = event => {
   const list = document.querySelector("#list");
   const listItem = document.createElement("li");
   const buttonDELETE = document.createElement("button");
+  const buttonEDIT = document.createElement("button");
 
   if (userInput.value.trim() === "") {
     alert("შეიყვანე რამე");
   } else {
     listItem.textContent = userInput.value;
+    buttonEDIT.textContent = "EDIT";
     buttonDELETE.textContent = "DELETE";
     listItem.append(buttonDELETE);
     list.append(listItem);
   }
 
+  buttonEDIT.addEventListener("click", renameItemHander);
   buttonDELETE.addEventListener("click", deleteMovieHandler);
 };
 
 const deleteMovieHandler = e => {
   e.target.parentNode.remove();
+};
+
+const renameItemHandler = e => {
+  e.target.parentNode.firstChild.textContent = prompt("change items name");
 };
